5cb2225fcfe014a937ff07c9ce672fbed0015452
Gabriellopes232 <gabriel.lopes.nascimento.dev@gmail.com>
2023-04-25 19:12:45 -0300

    feat use errors verification in server

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -3,10 +3,12 @@ import cors from 'cors';
 import routes from './routes'
 import { apiErrorValidator } from '../middlewares/error';
 import '@shared/typeorm';
+import { errors } from 'celebrate';
 
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
 
 app.use(routes);
@@ -16,3 +18,5 @@ app.use(apiErrorValidator);
 app.listen(3333, () => {
   console.log('Server started on port 3333!');
 });
+
+app.use(errors);
